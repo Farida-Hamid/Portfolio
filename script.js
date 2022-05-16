93de424e409922c2fc8410e628fd5e9263732368
Farida-Hamid <faridaaddala@gmail.com>
2022-05-16 17:21:27 +0300

    Build skeleton for the popup page

---
+++ script.js
@@ -0,0 +1,17 @@
+const header = document.querySelector('header');
+const section = document.createElement('section');
+header.appendChild(section);
+// section.innerHTML = `<p>Hello eorld</p>`;
+
+const open = document.querySelector('#menue-button');
+
+open.addEventListener('click',()=> {
+  const window = document.createElement('div');
+  window.className = 'window';
+  const popup = document.createElement('div');
+  popup.className = 'popup';
+  popup.innerHTML = `<div class="all"></div>`
+
+  window.appendChild(popup);
+  header.appendChild(window);
+})
