9f15a20fa969393a6c4a7b2f5b0aeea5e1298908
Farida-Hamid <faridaaddala@gmail.com>
2022-05-17 17:44:45 +0300

    Desktop window

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ open.addEventListener('click',()=> {
   <button type='button' class="close">X</button>
   <i class="fa-solid fa-xmark close"></i>
   <ul id="popup">
-  <li>Portifolio</li>
+  <li><a href=".project">Portifolio</a></li>
   <li>About</li>
   <li>Contact</li>
   </ul>
@@ -25,4 +25,42 @@ open.addEventListener('click',()=> {
   close.addEventListener('click', () => {
     header.removeChild(window);
   });
+})
+
+const main = document.querySelector('main');
+const project = document.createElement('project');
+main.appendChild(project);
+
+const showProject = document.querySelector('.see-project')
+
+showProject.addEventListener('click', () => {
+  const window = document.createElement('div');
+  window.className = 'window';
+  const projectPopup = document.createElement('div');
+  projectPopup.className = 'projectPopup';
+  projectPopup.innerHTML = `
+  <div class="container-desktop-popup">
+    <div class="desktop-window">
+      <div class="desktop-top-popup">
+        <h2>Tonic</h2>
+        <button type='button' class="close">X</button>
+      </div>
+      <ul class="tech">
+        <li class="company-name"><a>company</a></li>
+        <li><a href="#"><img src="images/Counter.png" alt="dot"></a></li>
+        <li><a>Back End Dev</a></li>
+        <li><a href="#"><img src="images/Counter.png" alt="dot"></a></li>
+        <li><a>2015</a></li>
+      </ul>
+      <img class="desktop-popup-snapshot" src="./images/Snapshoot-Portfolio-popup.svg" alt="project 1 snapshot">
+    </div>
+  </div>`;
+
+  window.appendChild(projectPopup);
+  header.appendChild(window);
+  const close = document.querySelector('.close');
+  close.addEventListener('click', () => {
+    header.removeChild(window);
+  });
+
 })
