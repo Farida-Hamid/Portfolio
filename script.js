d74bb47cc1e3912075644aac5fadb6d8b95592b7
Farida-Hamid <faridaaddala@gmail.com>
2022-05-19 10:33:42 +0300

    Add project content as a js objects

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Menu popup window
 const header = document.querySelector('body');
 const section = document.createElement('section');
 header.appendChild(section);
@@ -38,15 +39,69 @@ open.addEventListener('click', () => {
   });
 });
 
+// Project popup window
+
+const buttonOne = document.querySelectorAll('.show-project');
+
+const projects = [
+  {
+    title: 'Tonic',
+    devs: ['CANOPY', '•', 'Back End Dev', '•', '2015'],
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    tags: ['html', 'css', 'javascript'],
+    image: '/assets/projects/project-2.png',
+    liveButton: 'See Live',
+    sourceButton: 'See Source',
+    live: 'images/live-version2.svg',
+    source: 'images/live-version1.svg',
+  },
+  {
+    title: 'Multi-Post Stories',
+    devs: ['FACEBOOK', '•', 'Full Stack Dev', '•', '2015'],
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    tags: ['html', 'Ruby on rails', 'css', 'javascript'],
+    image: './images/Snapshoot-Portfolio-2.png',
+    liveButton: 'See Live',
+    sourceButton: 'See Source',
+    live: 'images/live-version2.svg',
+    source: 'images/live-version1.svg',
+  },
+  {
+    title: 'Facebook 360',
+    devs: ['FACEBOOK', '•', 'Full Stack Dev', '•', '2015'],
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+      tags: ['html', 'Ruby on rails', 'css', 'javascript'],
+      image: './images/Snapshoot-Portfolio-3.png',
+      liveButton: 'See Live',
+      sourceButton: 'See Source',
+      live: 'images/live-version2.svg',
+      source: 'images/live-version1.svg',
+  },
+  {
+  title: 'Uber Navigation',
+    devs: ['Uber', '•', 'Lead Developer', '•', '2018'],
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    tags: ['html', 'Ruby on rails', 'css', 'javascript'],
+    image: './images/Snapshoot-Portfolio4.png',
+    liveButton: 'See Live',
+    sourceButton: 'See Source',
+    live: 'images/live-version2.svg',
+    source: 'images/live-version1.svg',
+  },
+];
+
+const showProject = document.querySelector('.show-project');
+
 const main = document.querySelector('main');
 const project = document.createElement('project');
 main.appendChild(project);
-
-const showProject = document.querySelector('.show-project')
-
 showProject.addEventListener('click', () => {
-  const window2 = document.createElement('div');
-  window2.className = 'big-window';
+  const window = document.createElement('div');
+  window.className = 'big-window';
   const projectPopup = document.createElement('div');
   projectPopup.className = 'project-popup';
   projectPopup.innerHTML = `
@@ -65,10 +120,10 @@ showProject.addEventListener('click', () => {
       <img class="desktop-popup-snapshot" src="./images/Snapshoot1-Portfolio.svg" alt="project 1 snapshot">
   </div>`;
 
-  window2.appendChild(projectPopup);
-  main.appendChild(window2);
+  window.appendChild(projectPopup);
+  main.appendChild(window);
   const close = document.querySelector('.close-button');
     close.addEventListener('click', () => {
-    main.removeChild(window2);
+    main.removeChild(window);
   });
 })
