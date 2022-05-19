17bfb2268c01819baf809106c93769f255c6720b
Farida-Hamid <faridaaddala@gmail.com>
2022-05-19 08:17:16 +0300

    Import skeleton from old branch

--- script.js
+++ script.js
@@ -37,3 +37,38 @@ open.addEventListener('click', () => {
     header.removeChild(window);
   });
 });
+
+const main = document.querySelector('main');
+const project = document.createElement('project');
+main.appendChild(project);
+
+const showProject = document.querySelector('.show-project')
+
+showProject.addEventListener('click', () => {
+  const window2 = document.createElement('div');
+  window2.className = 'big-window';
+  const projectPopup = document.createElement('div');
+  projectPopup.className = 'project-popup';
+  projectPopup.innerHTML = `
+    <div class="desktop-window">
+      <div class="desktop-top-popup">
+        <h2>Tonic</h2>
+        <button type='button' class="close-button">X</button>
+        </div>
+      <ul class="tech">
+        <li class="company-name"><a>company</a></li>
+        <li><a href="#"><img src="images/Counter.png" alt="dot"></a></li>
+        <li><a>Back End Dev</a></li>
+        <li><a href="#"><img src="images/Counter.png" alt="dot"></a></li>
+        <li><a>2015</a></li>
+      </ul>
+      <img class="desktop-popup-snapshot" src="./images/Snapshoot1-Portfolio.svg" alt="project 1 snapshot">
+  </div>`;
+
+  window2.appendChild(projectPopup);
+  main.appendChild(window2);
+  const close = document.querySelector('.close-button');
+    close.addEventListener('click', () => {
+    main.removeChild(window2);
+  });
+})
