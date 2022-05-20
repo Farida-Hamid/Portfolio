aa01a1699e5a4f4970a56e74268b39ecf172a621
Farida-Hamid <faridaaddala@gmail.com>
2022-05-20 11:42:32 +0300

    Adjust error message

--- validation.js
+++ validation.js
@@ -1,13 +1,13 @@
 const email = document.getElementById('email');
 const form = document.getElementById('form');
-const errorElement = document.getElementById('error');
+const errorElement = document.getElementById('error-message');
 form.addEventListener('submit', (e) => {
   const messages = [];
   if (email.value.match(/^[a-z@.0-9-_]*$/)) {
     messages.push('your email is correct');
   } else {
     e.preventDefault();
-    messages.push('Enter the email in lower case to continue');
+    messages.push('Please enter the email in lower case.');
     errorElement.innerText = messages.join(', ');
   }
 });
