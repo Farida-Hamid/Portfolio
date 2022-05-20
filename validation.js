b8964d928a79d07fbf3773bcc3368a788bf91c6a
Farida-Hamid <faridaaddala@gmail.com>
2022-05-20 11:30:10 +0300

    Add skeleton for error check

--- validation.js
+++ validation.js
@@ -1,3 +1,6 @@
 const email = document.getElementById('email');
 const form = document.getElementById('form');
 const errorElement = document.getElementById('error');
+form.addEventListener('submit', (e) => {
+  const messages = [];
+});
