f95165075115538524ca7a6358d49d54c130a771
Farida-Hamid <faridaaddala@gmail.com>
2022-05-20 11:31:07 +0300

    Check if input email is in the right format

--- validation.js
+++ validation.js
@@ -3,4 +3,7 @@ const form = document.getElementById('form');
 const errorElement = document.getElementById('error');
 form.addEventListener('submit', (e) => {
   const messages = [];
+  if (email.value.match(/^[a-z@.0-9-_]*$/)) {
+    messages.push('your email is correct');
+  }
 });
