4154c5c8a1b31a0dc75ba7d82607e8b2077709a2
Farida-Hamid <faridaaddala@gmail.com>
2022-05-21 13:24:47 +0300

    Retrieve storage

--- storage.js
+++ storage.js
@@ -1,8 +1,18 @@
-const name = document.querySelector('#name');
+const fullName = document.querySelector('#name');
 const email = document.querySelector('#email');
 const message = document.querySelector('#text');
 
-function storage(name, email, message) {
-  const saver = JSON.stringify({name, email, message});
+function storage(fullName, email, message) {
+  const saver = JSON.stringify({fullName, email, message});
   localStorage.setItem('data', saver);
+}
+
+function retrieve(){
+  const data = JSON.parse(localStorage.getItem('data'));
+
+  if(data){
+    fullName.value = data.fullName;
+    email.value = data.email;
+    message.value = data.message;
+  }
 }
