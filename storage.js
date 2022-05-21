ff686f79eb335dd8262befdfbfccb6b3fa4489e6
Farida-Hamid <faridaaddala@gmail.com>
2022-05-21 13:39:51 +0300

    Fix linter errors

--- storage.js
+++ storage.js
@@ -3,14 +3,14 @@ const email = document.querySelector('#email');
 const message = document.querySelector('#text');
 
 function storage(fullName, email, message) {
-  const saver = JSON.stringify({fullName, email, message});
+  const saver = JSON.stringify({ fullName, email, message });
   localStorage.setItem('data', saver);
 }
 
-function retrieve(){
+function retrieve() {
   const data = JSON.parse(localStorage.getItem('data'));
 
-  if(data){
+  if (data) {
     fullName.value = data.fullName;
     email.value = data.email;
     message.value = data.message;
